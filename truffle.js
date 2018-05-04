9c8d09a74f761f1f6a03dcdffd425eecfc1e67eb
apanizo <adolfo.panizo>
2018-05-04 15:52:21 +0200

    Adding config files for deploying contracts to rinkeby kovan and local testnet

--- truffle.js
+++ truffle.js
@@ -1,5 +1,15 @@
-module.exports = {
+// @flow
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
+
+const config = {
   networks: {
+    rinkeby: {
+      network_id: 4
+    },
+    kovan: {
+      network_id: 42
+    },
     development: {
       host: "localhost",
       port: 8545,
@@ -8,3 +18,20 @@ module.exports = {
     }
   }
 };
+
+const _ = require("lodash");
+
+try {
+  _.merge(config, require("./truffle-local"));
+  console.log(config)
+} catch (e) {
+  if (e.code === "MODULE_NOT_FOUND") {
+    // eslint-disable-next-line no-console
+    console.log("No local truffle config found. Using all defaults...");
+  } else {
+    // eslint-disable-next-line no-console
+    console.warn("Tried processing local config but got error:", e);
+  }
+}
+
+module.exports = config;
